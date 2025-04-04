b213833ac5137535841a71f9aa4dc9e4234d3de3
cjur3 <cjure89@gmail.com>
2025-04-04 14:07:12 +0200

    Improve filter so it only advances if there is an open job (crafting/building) assigned to a facility

--- scripts/bastion-time.js
+++ scripts/bastion-time.js
@@ -10,6 +10,37 @@ function differenceInDays(diff, secondsInDay, currentDaySeconds) {
     }
 }
 
+function areRequirementsForBastionProgressMet(actor) {
+    if (actor.type !== "character") {
+        return false;
+    }
+
+    if (actor.system && actor.system.details && actor.system.details.level < 5) {
+        return false;
+    }
+
+    if (actor.items) {
+        const facilities = actor.items.filter(i => i.type === "facility");
+
+        for (facilityId in facilities) {
+            const facility = facilities[facilityId];
+
+            if (facility.system) {
+                const { building, progress } = facility.system;
+
+                if (building && !building.built) {
+
+                    if (progress && progress.max) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    return false;
+}
 
 Hooks.on("ready", () => {
     console.log("initializing bastion time");
@@ -18,9 +49,9 @@ Hooks.on("ready", () => {
         const { time } = SimpleCalendar.api.getCurrentCalendar();
         const secondsInDay = time.secondsInMinute * time.minutesInHour * time.hoursInDay;
         const currentDaySeconds = date.hour * time.minutesInHour * time.secondsInMinute + date.minute * time.secondsInMinute + date.second;
-    
+
         const difference = differenceInDays(diff, secondsInDay, currentDaySeconds);
-        const playerActors = game.actors.filter(a => a.type === "character" && a.system && a.system.details && a.system.details.level >= 5);
+        const playerActors = game.actors.filter(areRequirementsForBastionProgressMet);
         playerActors.forEach(actor => {
             game.system.bastion.advanceAllFacilities(actor, { duration: difference })
         });
